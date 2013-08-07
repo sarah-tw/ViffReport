85f4e0d11ec26002c764b7cc96b74271fe7d7a72
Si Xiaojing <sixiaojing1988@gmail.com>
2013-08-07 17:40:03 +0800

    refactor controller js file

--- js/controllers.js
+++ js/controllers.js
@@ -31,101 +31,103 @@ angular.module('viffReport', [])
 
       $scope.approveDiff = function(){
         $scope.currentViff.misMatchPercentage = 0;
-      }
-
-      key('/', function(){
-        $scope.$apply(function () {
-          $("#search_input").focus();
-        });
-        return false;
-      });
-
-      
-      key('A, ctrl+A', function(){
-        $scope.$apply(function () {
-          $scope.approveDiff();
-        });
-        return false;
-      });
-
-      key('j', function () {
-        $scope.$apply(function () {
-          var currentBrowserCases = filter($scope.cases, function(item){ return item.browser == $scope.search.browser})
-          var idx = currentBrowserCases.indexOf($scope.currentViff);
-          if(idx != currentBrowserCases.length - 1) {
-            $scope.currentViff = currentBrowserCases[idx + 1];
-            $scope.currentViffId = $scope.currentViff.id;
-          }
-          return false;
-        });
-      });
-
-      key('k', function () {
-        $scope.$apply(function () {
-          var currentBrowserCases = filter($scope.cases, function(item){ return item.browser == $scope.search.browser})
-          var idx = currentBrowserCases.indexOf($scope.currentViff);
-          if(idx != 0) {
-            $scope.currentViff = currentBrowserCases[idx - 1];
-            $scope.currentViffId = $scope.currentViff.id;
-          }
-          return false;
-        });
-      });
-
-      key('h', function () {
-        $scope.$apply(function () {
-          var idx = $scope.browsers.indexOf($scope.search.browser);
-          if(idx != 0) {
-            $scope.search.browser = $scope.browsers[idx - 1];
-          }
-          return false;
-        });
-      });
-
-      key('l', function () {
-        $scope.$apply(function () {
-          var idx = $scope.browsers.indexOf($scope.search.browser);
-          if(idx != $scope.browsers.length - 1) {
-            $scope.search.browser = $scope.browsers[idx + 1];
-          }
-          return false;
-        });
-      });
-
-      key('f', function () {
-        $scope.$apply(function () {
-          $scope.search.browser = "firefox"
-          return false;
-        });
-      });
-      key('c', function () {
-        $scope.$apply(function () {
-          $scope.search.browser = "chrome"
-          return false;
-        });
-      });
-      key('i', function () {
-        $scope.$apply(function () {
-          $scope.search.browser = "ie"
-          return false;
-        });
-      });
-      key('o', function () {
-        $scope.$apply(function () {
-          $scope.search.browser = "opera"
-          return false;
-        });
-      });
-
-      key('s', function () {
-        $scope.$apply(function () {
-          $scope.search.browser = "safari"
-          return false;
-        });
-      });
+      }      
 
       $scope.$apply();
-    }
+    };
+
+
+    key('/', function(){
+      $scope.$apply(function () {
+        $("#search_input").focus();
+      });
+      return false;
+    });
+
+    
+    key('A, ctrl+A', function(){
+      $scope.$apply(function () {
+        $scope.approveDiff();
+      });
+      return false;
+    });
+
+    key('j', function () {
+      $scope.$apply(function () {
+        var currentBrowserCases = filter($scope.cases, function(item){ return item.browser == $scope.search.browser})
+        var idx = currentBrowserCases.indexOf($scope.currentViff);
+        if(idx != currentBrowserCases.length - 1) {
+          $scope.currentViff = currentBrowserCases[idx + 1];
+          $scope.currentViffId = $scope.currentViff.id;
+        }
+        return false;
+      });
+    });
+
+    key('k', function () {
+      $scope.$apply(function () {
+        var currentBrowserCases = filter($scope.cases, function(item){ return item.browser == $scope.search.browser})
+        var idx = currentBrowserCases.indexOf($scope.currentViff);
+        if(idx != 0) {
+          $scope.currentViff = currentBrowserCases[idx - 1];
+          $scope.currentViffId = $scope.currentViff.id;
+        }
+        return false;
+      });
+    });
+
+    key('h', function () {
+      $scope.$apply(function () {
+        var idx = $scope.browsers.indexOf($scope.search.browser);
+        if(idx != 0) {
+          $scope.search.browser = $scope.browsers[idx - 1];
+        }
+        return false;
+      });
+    });
+
+    key('l', function () {
+      $scope.$apply(function () {
+        var idx = $scope.browsers.indexOf($scope.search.browser);
+        if(idx != $scope.browsers.length - 1) {
+          $scope.search.browser = $scope.browsers[idx + 1];
+        }
+        return false;
+      });
+    });
+
+    key('f', function () {
+      $scope.$apply(function () {
+        $scope.search.browser = "firefox"
+        return false;
+      });
+    });
+    key('c', function () {
+      $scope.$apply(function () {
+        $scope.search.browser = "chrome"
+        return false;
+      });
+    });
+    key('i', function () {
+      $scope.$apply(function () {
+        $scope.search.browser = "ie"
+        return false;
+      });
+    });
+
+    key('o', function () {
+      $scope.$apply(function () {
+        $scope.search.browser = "opera"
+        return false;
+      });
+    });
+
+    key('s', function () {
+      $scope.$apply(function () {
+        $scope.search.browser = "safari"
+        return false;
+      });
+    });
 
     function filter(arr, condition) {
       var results = [];
