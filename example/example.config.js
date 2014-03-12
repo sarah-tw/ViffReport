1ac738a32f77fbea1b4d7b548c6d35ee3d5e3d5f
Wang Qiu <winsonwq@gmail.com>
2014-03-12 08:40:58 +0800

    set 8000 as default port

--- example/example.config.js
+++ example/example.config.js
@@ -10,8 +10,8 @@ module.exports = {
   seleniumHost: 'http://localhost:4444/wd/hub',
   browsers: ['firefox'],
   envHosts: {
-    build: 'http://localhost:4000/example/build',
-    prod: 'http://localhost:4000/example/prod'
+    build: 'http://localhost:8000/example/build',
+    prod: 'http://localhost:8000/example/prod'
   },
   paths: [
     { 'Tiny CSS Difference': ['/tiny_css_difference.html', size(800)] },
