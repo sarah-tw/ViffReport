e2b21a00ec05a519af3a41b47e2ae82445d7ced7
OwenYang <usay@outlook.com>
2014-03-17 13:02:18 +0800

    optimise user experience while scrollinig

--- js/directives.js
+++ js/directives.js
@@ -4,6 +4,8 @@ angular.module('viffReport')
 
       angular.element($window).bind("scroll", function () {
         var DIFF_IMAGE_SELECTOR = $(".diffImage img");
+        var WINDOW_HEIGHT = $(window).height();
+        var DIFF_VISUAL_HEIGHT = WINDOW_HEIGHT - (43 + 35 + 60);
 
         var offset_y = $(document).scrollTop();
         var diffImageHeight = DIFF_IMAGE_SELECTOR.get(0).height;
@@ -11,7 +13,7 @@ angular.module('viffReport')
           height: element[0].height,
           parentHeight: element[0].parentElement.clientHeight
         };
-        var offset_value = parseInt(offset_y / $(document).height() * envImage.height);
+        var offset_value = parseInt(offset_y / diffImageHeight * envImage.height);
 
         // when scroll bar at the top of window
         if (offset_y <= 10 || envImage.parentHeight > envImage.height) {
@@ -25,6 +27,9 @@ angular.module('viffReport')
           return element[0].style.top = integerToPixel(imagePosition);
         }
 
+        var blankHeight = DIFF_VISUAL_HEIGHT - (envImage.parentHeight * diffImageHeight / envImage.height);
+        $(".blank").height(blankHeight);
+
         element[0].style.top = integerToPixel(0 - offset_value);
         scope.$apply();
       });
@@ -36,6 +41,5 @@ angular.module('viffReport')
 
         return imagePosition;
       }
-
     }
   });
